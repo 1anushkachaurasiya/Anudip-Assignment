073693b780b15001225df5158fe38718073f794f
1anushkachaurasiya <120013303+1anushkachaurasiya@users.noreply.github.com>
2022-12-09 17:49:06 +0530

    Add files via upload

---
+++ AnushkaProject/src/GreatestNumber.java
@@ -0,0 +1,29 @@
+ public class GreatestNumber {
+
+	public static void main(String[] args) {
+		int a=17, b=53, c=10;
+		greatestnum(a,b,c);
+	 
+	}
+	public static void greatestnum(int a,int b,int c) {
+		if(a>b) {
+			if(a>c) {
+				System.out.println("Greater Number is :"+a);
+			}
+			else {
+				System.out.println("Greater Number is :"+c);
+			}
+			
+		}
+		else {
+			if(b>c) {
+				System.out.println("Greater Number is :"+b);
+			}
+			else {
+				System.out.println("Greater Number is :"+c);
+			}
+		}
+		
+	}
+
+}
