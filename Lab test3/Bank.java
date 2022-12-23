d5fb0d0523335ef00ce9033db16d42868e8ae865
1anushkachaurasiya <120013303+1anushkachaurasiya@users.noreply.github.com>
2022-12-23 22:34:12 +0530

    Add files via upload

---
+++ Lab test3/Bank.java
@@ -0,0 +1,7 @@
+// abstract class For Bank
+abstract class Bank
+ {
+	// abstract method for getting the balance
+	
+abstract int getBalance();
+}
---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+	// Data members of the Person class
+	String name;
+	int age;
+	String phone;
+	// parameterized constructor for the person class
+	Person(String name, int age, String phone){
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+	}
+}
